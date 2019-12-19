9806462cf4db454aaf6224aa4e8ca37206e4769a
lli-trinity <31329073+lli-trinity@users.noreply.github.com>
2019-12-18 22:55:57 -0600

    Update correlation.py

--- src/correlation.py
+++ src/correlation.py
@@ -4,8 +4,8 @@ from scipy import stats
 def calculate_correlation(data):
     spearman_result = [] #list that store return spearman correlation result
     pearson_result = [] #list that store return pearson correlation result
-    age = data[:,0]
-    for i in range(1,len(data[0])):
+    age = data[:,0] #select age column from imput data
+    for i in range(1,len(data[0])): #
         s_rho,pvalue = stats.spearmanr(data[:,i],age)
         p_rho,pvalue = stats.pearsonr(data[:,i],age)
         spearman_result.append(s_rho)
